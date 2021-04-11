376897d833278a3439cce2b011b0c127f95d3954
Bryg9 <bryg9@users.noreply.github.com>
2021-04-11 11:34:04 +0000

    Poprawka linta

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -36,8 +36,11 @@ def format_to_json(msg, imie):
     y = json.dumps(x)
     return y
 
+
 def format_to_xml(msg, imie):
-    return '<greetings><name>Brygida</name> <msg>Hello World!</msg></greetings>'
+    return '<greetings><name>Brygida</name> <msg> \
+            Hello World!</msg></greetings>'
+
 
 def plain_text(msg, imie):
     return imie + ' ' + msg
